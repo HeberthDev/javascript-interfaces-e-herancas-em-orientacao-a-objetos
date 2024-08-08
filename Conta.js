be8180cdd80869753186d1f5720578557b763634
HeberthDev <heberthdeveloper@gmail.com>
2024-08-08 07:42:21 -0300

    Herança

--- Conta.js
+++ Conta.js
@@ -1,28 +1,47 @@
 export class Conta {
-    constructor(saldoInicial, cliente, agencia) {
+    constructor( saldoInicial, cliente, agencia ) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
-        this._agencia = agencia; 
+        this._agencia = agencia;
     }
 
-    sacar(valor){
-        taxa = 1.1 * valor;
-        
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+    set cliente( novoValor ) {
+        if ( novoValor instanceof Cliente ) {
+            this._cliente = novoValor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    sacar( valor ){
+        let taxa = 1;
+
+        if ( this._tipo == 'corrente' ) {
+            taxa = 1.1;
+        }
+
+        const valorSacado = taxa * valor;
+        
+        if ( this._saldo >= valor ) {
+            this._saldo -= valor;
+            return valorSacado;
+        }
+    }
+
+    depositar( valor ){
+        if ( valor <= 0 ) {
             return;
         } 
         this._saldo += valor;           
     }
 
-    tranferir(valor, conta){
+    tranferir( valor, conta ) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
