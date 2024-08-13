9e877bcd763f50d93f9a0f21b9adfea4af2d8396
HeberthDev <heberthdeveloper@gmail.com>
2024-08-13 07:53:50 -0300

    Classes Abstratas

--- Conta.js
+++ Conta.js
@@ -1,12 +1,14 @@
+// Classe abstrata
 export class Conta {
     constructor( saldoInicial, cliente, agencia ) {
+        if( this.constructor == Conta ) { // Quando o desenvolvedor der "new Conta" ele vai reconhecer que está se referenciando ao construtor Conta e vai lançar esse erro.
+            throw new Error( "Você não deveria instanciar um objeto do tipo Conta diretamente." );
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
 
-        if( this.constructor == Conta ) {
-            console.log( "Você não deveria instanciar um objeto do tipo conta." );
-        }
     }
 
     set cliente( novoValor ) {
