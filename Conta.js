896ccf8bbc81cdcfdd14b2330a2d3763abac9ac7
HeberthDev <heberthdeveloper@gmail.com>
2024-08-13 07:39:57 -0300

    Propriedade constructor

--- Conta.js
+++ Conta.js
@@ -3,6 +3,10 @@ export class Conta {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+
+        if( this.constructor == Conta ) {
+            console.log( "Você não deveria instanciar um objeto do tipo conta." );
+        }
     }
 
     set cliente( novoValor ) {
@@ -43,7 +47,7 @@ export class Conta {
     }
 
     tranferir( valor, conta ) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
+        const valorSacado = this.sacar( valor );
+        conta.depositar( valorSacado );
     }
 }
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente1 = new Cliente( "Ricardo", 11122233309 );
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001 );
+contaCorrenteRicardo.depositar( 500 );
+contaCorrenteRicardo.sacar( 100 );
 
 const contaPoupanca = new ContaPoupanca( 50, cliente1, 1001 );
+const conta = new Conta( 0, cliente1, 1001 );
 
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
+console.log( contaPoupanca );
+console.log( contaCorrenteRicardo ); 
+console.log( conta ); 
