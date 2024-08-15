cbff2a76e5fc7fc71822756dffe09677b391c2f3
HeberthDev <heberthdeveloper@gmail.com>
2024-08-15 08:44:17 -0300

    Duck Type

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,4 +1,7 @@
-// Ser autenticavel significa ter o método "autenticar()"
+/* 
+    Ser autenticavel significa ter o método "autenticar()".
+    Duck type - Em sentido literal quer dizer que é do tipo pato e em significado se refere que se anda com um pato, se ele faz "quac-quac" então ele deve ser um pato, ou seja, se tem uma determinada propriedade que precisa, uma determinada característica que precisa já é o suficiente independente do tipo.
+*/
 export class SistemaAutenticacao {
     static login( autenticavel, senha ){
         if (SistemaAutenticacao.ehAutenticavel( autenticavel )) {
