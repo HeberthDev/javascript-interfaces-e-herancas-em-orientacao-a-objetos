41c8ded1d6b8f730f9c8be973ed592d7e547e6f5
HeberthDev <heberthdeveloper@gmail.com>
2024-08-15 07:58:12 -0300

    Polimorfismo

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import { Cliente } from "./Conta/Cliente.js"
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+// import { Cliente } from ".Cliente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente( "Ricardo", 11122233309 );
+const diretor = new Diretor( "Rodrigo", 10000, 12345678900 );
+diretor.cadastrarSenha("123456");
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001 );
-const contaPoupanca = new ContaPoupanca( 50, cliente1, 1001 );
-const contaSalario = new ContaSalario( cliente1 );
+const gerente = new Gerente( "Ricardo",  5000, 12378945601 );
+gerente.cadastrarSenha("123");
 
-contaSalario.depositar( 100 );
-contaSalario.sacar( 10 );
+const estaLogado = SistemaAutenticacao.login( gerente, "123" );
 
-console.log( contaSalario ); 
+console.log( estaLogado );
