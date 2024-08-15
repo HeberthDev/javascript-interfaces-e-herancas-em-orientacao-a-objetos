a949f7928028258250622a4d1822f0895c168918
HeberthDev <heberthdeveloper@gmail.com>
2024-08-15 08:17:49 -0300

    Interfaces

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// import { Cliente } from ".Cliente.js";
+import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionario/Gerente.js";
 import { Diretor } from "./Funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+
+const cliente = new Cliente( "Lais", 78945612379, "456" );
 
 const diretor = new Diretor( "Rodrigo", 10000, 12345678900 );
 diretor.cadastrarSenha("123456");
@@ -9,6 +11,10 @@ diretor.cadastrarSenha("123456");
 const gerente = new Gerente( "Ricardo",  5000, 12378945601 );
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login( gerente, "123" );
+const clienteEstaLogado = SistemaAutenticacao.login( cliente, "456" );
+const gerenteEstaLogado = SistemaAutenticacao.login( gerente, "123" );
+const diretorEstaLogado = SistemaAutenticacao.login( diretor, "123456" );
 
-console.log( estaLogado );
+console.log( "clienteEstaLogado: ", clienteEstaLogado );
+console.log( "gerenteEstaLogado: ", gerenteEstaLogado );
+console.log( "diretorEstaLogado: ", diretorEstaLogado );
